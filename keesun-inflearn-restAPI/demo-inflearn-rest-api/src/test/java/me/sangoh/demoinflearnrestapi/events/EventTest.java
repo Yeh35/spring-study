26007ddcbd0f028274bce351c7a103b4bfe0e2ab
yehsangoh <jaws0910@gmail.com>
2020-06-18 23:26:50 +0900

    Event Entity 추가

---
+++ keesun-inflearn-restAPI/demo-inflearn-rest-api/src/test/java/me/sangoh/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,37 @@
+package me.sangoh.demoinflearnrestapi.events;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertNotNull(event);
+    }
+
+    @Test
+    public void javaBean() {
+        //given
+        Event event = new Event();
+        String bean_event = "bean Event";
+        String spring = "Spring";
+
+        //when
+        event.setName(bean_event);
+        event.setDescription(spring);
+
+        //then
+        assertNotNull(event);
+        assertEquals(bean_event, event.getName());
+        assertEquals(spring, event.getDescription());
+    }
+
+}
