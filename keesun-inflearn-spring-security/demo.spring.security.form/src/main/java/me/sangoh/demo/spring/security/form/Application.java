1a2fdaafc31253122e199a1c6b41b6f9827099ee
jaws0227 <jaws0910@gmail.com>
2020-11-23 11:04:07 +0900

    실습용 프로젝트 생성

---
+++ keesun-inflearn-spring-security/demo.spring.security.form/src/main/java/me/sangoh/demo/spring/security/form/Application.java
@@ -0,0 +1,13 @@
+package me.sangoh.demo.spring.security.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
