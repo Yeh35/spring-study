5ebb44eee86b0a443f3ad326e3d5e64730c0ad33
jaws0227 <jaws0910@gmail.com>
2020-11-24 16:40:40 +0900

    스프링 시큐리티 아키택쳐 연습

---
+++ keesun-inflearn-spring-security/demo.spring.security.form/src/main/java/me/sangoh/demo/spring/security/form/form/SampleService.java
@@ -0,0 +1,20 @@
+package me.sangoh.demo.spring.security.form.form;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class SampleService {
+
+    public void dashboard() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Object cradential = authentication.getCredentials();
+        Boolean authenticaticated = authentication.isAuthenticated();
+    }
+}
